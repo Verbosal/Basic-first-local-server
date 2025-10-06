bce63e5dc38b9b72bc402308ff30414cda600ed5
Verbosal <lukaszplfajnyy@gmail.com>
2025-10-06 06:39:32 +0000

    Added path

--- src/path_handlers.js
+++ src/path_handlers.js
@@ -19,6 +19,14 @@ const pathConfigs = [
       res.end("hello world!\n");
     },
   },
+  {
+    path: "/favicon.ico",
+    allowed_methods: ["GET"],
+    handler: (req, res) => {
+      res.writeHead(200, { "Content-Type": "image/x-icon" });
+      res.end("Favicon get!\n");
+    },
+  },
 ];
 
 export function handlePath(path, req, res) {
